7898e112a70b7eea80e583d025b12ed53e8cae99
gdemartino93 <g.demartino9393@gmail.com>
2025-03-01 00:41:28 +0100

    passing parameter to a command

--- MVVM/ViewModels/CommandsViewModel.cs
+++ MVVM/ViewModels/CommandsViewModel.cs
@@ -10,15 +10,23 @@ namespace MAUI_MVVM_Pattern.MVVM.ViewModels
     public class CommandsViewModel
     {
         public ICommand SaveBtnClickedCommand { get; }
+        public ICommand SearchCommand { get; }
         public CommandsViewModel()
         {
             SaveBtnClickedCommand = new Command(ExecuteSaveCommand);
+            SearchCommand = new Command<string>(ExecuteSearch);
         }
 
         private void ExecuteSaveCommand()
         {
             App.Current.MainPage.DisplayAlert("title", "message", "ok");
         }
+
+        private void ExecuteSearch(string searchText)
+        {
+            App.Current.MainPage.DisplayAlert("ricerca", "ricerca", "ok");
+        }
+
 
     }
 }
